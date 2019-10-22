018b6e92fc994f1933716d1a43b6bc28ab2168bf
DiquiS <bartek.kazior12@wp.pl>
2019-10-22 02:37:45 +0200

    working with numbers

---
+++ numbers.py
@@ -0,0 +1,13 @@
+print(10 % 4)
+my_num = -5
+print(str(my_num) + " times a week")
+print(abs(my_num))
+print(pow(3, 2))
+print(max(129, 12458))
+print(min(14, 124))
+print(round(124.5))
+
+from math import *
+print(floor(3745.19284))
+print(ceil(312.151))
+print(sqrt(36))
