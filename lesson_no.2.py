3332aa138498d60fb152c5b679178d518d8940df
DiquiS <bartek.kazior12@wp.pl>
2019-10-22 02:18:56 +0200

    Working with strings

---
+++ lesson_no.2.py
@@ -0,0 +1,12 @@
+phrase = "Learning string"
+print("Learning\nstrin")
+print("Learning \"string\"")
+print(phrase + "is cool")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print([0])
+print(phrase.index("str"))
+print(phrase.replace("Learning", "Studying"))
